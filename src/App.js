4cd89bdf76cf069674b7bdffceef8a8329461548
maduiep <emmanuelmaduip@gmail.com>
2020-10-22 11:59:12 +0100

    adding faq page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Homepage from "./pages/Home/Home";
-import Learnable from "./pages/Learnable/Learnable";
-import Developer from "./pages/Developer/Developer";
-import Designer from "./pages/Designer/Designer";
+//import Homepage from "./pages/Home/Home";
+//import Learnable from "./pages/Learnable/Learnable";
+//import Developer from "./pages/Developer/Developer";
+// import Designer from "./pages/Designer/Designer";
+import Faq from "./pages/Faq/Faq";
+
 
 // pages import
 
@@ -12,10 +14,8 @@ const App = () => {
         <main id="app">
             <Router>
                 <Switch>
-                    <Route path="/"Homepage={Homepage} />
-                    <Route path="/"Learnable={Learnable} />
-                    <Route path="/"Developer={Developer} />
-                    <Route path="/"Designer={Designer} />
+                    
+                    <Route path="/" component={Faq} />
                 </Switch>
             </Router>
         </main>
---
+++ src/pages/Faq/Faq.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Navbar from "../../components/navigation/Navbar/Navbar";
+import Nav from "../../components/navigation/Nav/Nav";
+import FAQ from "../../components/section/FAQ/FAQ";
+import Subscribe from "../../components/section/Subscribe/Subscribe";
+import Footer from "../../components/section/Footer/Footer";
+
+const Faq = () => {
+ return (
+  <>
+   <Navbar />
+   <Nav />
+   <FAQ />
+   <Subscribe />
+   <Footer />
+  </>
+ )
+}
+
+export default Faq;
