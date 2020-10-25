d05ff7d3c8d84c4eedf31d2afbe5825fbfb9ece0
maduiep <emmanuelmaduip@gmail.com>
2020-10-25 17:13:53 +0100

    updating conflicts

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-<<<<<<< HEAD
-import Homepage from "./pages/Home/Home";
-// import Learnable from "./pages/Learnable/Learnable";
-//import Developer from "./pages/Developer/Developer";
-// import Designer from "./pages/Designer/Designer";
-// import JournalPage from "./pages/JournalPage/JournalPage";
-// import Apply from "./pages/Apply/Apply";
-// import Apply from "./pages/Faq/Faq";
 
-=======
 import HomePage from "./pages/Home/Home";
 import LearnablePage from "./pages/Learnable/Learnable";
 import DeveloperPage from "./pages/Developer/Developer";
 import DesignerPage from "./pages/Designer/Designer";
 import JournalPage from "./pages/JournalPage/JournalPage";
 import ApplyPage from "./pages/Apply/Apply";
->>>>>>> 2b024a5f5c9605827951a36391cfd9fe079f1eb1
+import ApplyPage from "./pages/Faq/Faq";
+
 
 
 // pages import
@@ -26,17 +18,13 @@ const App = () => {
         <main id="app">
             <Router>
                 <Switch>
-<<<<<<< HEAD
-                    
-                    <Route path="/" component={Homepage} />
-=======
                     <Route path="/" exact component={HomePage} />
                     <Route path="/learnable" component={LearnablePage} />
                     <Route path="/apply" component={ApplyPage} />
+                    <Route path="/faq" component={Faq} />
                     <Route path="/journal" component={JournalPage} />
                     <Route path="/designer" component={DesignerPage} />
                     <Route path="/developer" component={DeveloperPage} />
->>>>>>> 2b024a5f5c9605827951a36391cfd9fe079f1eb1
                 </Switch>
             </Router>
         </main>
